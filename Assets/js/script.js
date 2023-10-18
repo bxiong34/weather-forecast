7635966f09099d601eaae284ece9677580cb6643
14089 <baoxiong814@gmail.com>
2023-10-18 12:38:16 -0500

    updated 5-day forecast, adding name and date

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,24 @@
 var apiKey = "8442b51bde1d703e3c6b958d0e487f4d";
 var city = "";
 var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=";
-var searchInput = document.querySelector(".search input");
+var searchInput = document.querySelector(".search-body input");
 var searchBtn = document.querySelector(".search btn");
 var weatherImg = document.getElementById(".weather-img")
 
 var today = dayjs();
-$('.date').text(today.format("ddd, MMMM D, YYYY"));
+  $(".date").text(today.format("ddd MMMM D, YYYY"));
+
+//days of weather
+var day1 = today.add(1, 'day');
+  $(".date1").text(day1.format("ddd MMMM D, YYYY"));
+var day2 = today.add(2, 'day');
+  $(".date2").text(day2.format("ddd MMMM D, YYYY"));
+var day3 = today.add(3, 'day');
+  $(".date3").text(day3.format("ddd MMMM D, YYYY"));
+var day4 = today.add(4, 'day');
+  $(".date4").text(day4.format("ddd MMMM D, YYYY"));
+var day5 = today.add(5, 'day');
+  $(".date5").text(day5.format("ddd MMMM D, YYYY"));
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -27,54 +39,57 @@ async function fetchWeather(city) {
 
   console.log(data);
   //gets data from api to append to current day on browser
+  // document.querySelector(".icon").innerHTML = data.list[0].weather[0].icon;
   document.querySelector(".city").innerHTML = data.city.name; 
+  // document.querySelector(".date1").innerHTML = (day1);
   document.querySelector(".temperature").innerHTML = "Temperature: " + Math.round(data.list[0].main.temp) + "°F";
   document.querySelector(".description").innerHTML = "Description: " + data.list[0].weather[0].description;
   document.querySelector(".wind").innerHTML = "Wind: " + Math.round(data.list[0].wind.speed) + "Mph";
   document.querySelector(".humidity").innerHTML = "Humidity: " + data.list[0].main.humidity + "%";
-  
-  // if (data.list[0].weather[0].main == "Clear"){
-  //   weatherImg.scr = "htps://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj7pmzNCftryAfpa1YBSzVeYtjgxDQnw09Ug0HVV47J8GEtHPYTH9hJgZ2M1k0YgE0pcZ1qekr4C14zyPCiVuQAfXLClK8Ww3hYB6v77yElP7Lo5BnUKo4n-w6yB17FAbw51WST6YKS0GMwyA4fYNxOZxEyNL6HhUfFRgVhOW0GyRdBRriMHFQ-qfh4cA/s320/sun.png";
-  // } 
-  //   else if (data.weather[0].main == "Clouds"){
-  //     weatherImg = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiwFTkt5z_dxU6w1UnS1PxiZV3HDiPGsAW5Lrsp09MnlCmkQre9GzO8MnGytaaY1eZoqBN6SMJ4U578_uDtiuXswovr1T3o-Kt5KK0mlN_zC0RDodJFaKHQ3Uk-HIZ3vuMvAKNJi8DDFwWA7F6BOxz78Oh-UePwJTuc3PG0ZIZypPE1xlMPl5z46joaEw/s320/Clouds.png";
-  //     }
-
 
   //gets data from api to append to 1st day on browser
-  document.querySelector(".city").innerHTML = data.city.name; 
+  document.querySelector(".city1").innerHTML = data.city.name; 
   document.querySelector(".temperature1").innerHTML = "Temperature: " + Math.round(data.list[2].main.temp) + "°F";
   document.querySelector(".description1").innerHTML = "Description: " + data.list[1].weather[0].description;
   document.querySelector(".wind1").innerHTML = "Wind: " + Math.round(data.list[1].wind.speed) + "Mph";
   document.querySelector(".humidity1").innerHTML = "Humidity: " + data.list[1].main.humidity + "%";
 
   //gets data from api to append to 2nd day on browser
-  document.querySelector(".city").innerHTML = data.city.name; 
+  document.querySelector(".city2").innerHTML = data.city.name; 
   document.querySelector(".temperature2").innerHTML = "Temperature: " + Math.round(data.list[2].main.temp) + "°F";
   document.querySelector(".description2").innerHTML = "Description: " + data.list[2].weather[0].description;
   document.querySelector(".wind2").innerHTML = "Wind: " + Math.round(data.list[2].wind.speed) + "Mph";
   document.querySelector(".humidity2").innerHTML = "Humidity: " + data.list[2].main.humidity + "%";
 
   //gets data from api to append to 3rd day on browser
-  document.querySelector(".city").innerHTML = data.city.name; 
+  document.querySelector(".city3").innerHTML = data.city.name; 
   document.querySelector(".temperature3").innerHTML = "Temperature: " + Math.round(data.list[1].main.temp) + "°F";
   document.querySelector(".description3").innerHTML = "Description: " + data.list[3].weather[0].description;
   document.querySelector(".wind3").innerHTML = "Wind: " + Math.round(data.list[3].wind.speed) + "Mph";
   document.querySelector(".humidity3").innerHTML = "Humidity: " + data.list[3].main.humidity + "%";
 
   //gets data from api to append to 4th day on browser
-  document.querySelector(".city").innerHTML = data.city.name; 
+  document.querySelector(".city4").innerHTML = data.city.name; 
   document.querySelector(".temperature4").innerHTML = "Temperature: " + Math.round(data.list[1].main.temp) + "°F";
   document.querySelector(".description4").innerHTML = "Description: " + data.list[4].weather[0].description;
   document.querySelector(".wind4").innerHTML = "Wind: " + Math.round(data.list[4].wind.speed) + "Mph";
   document.querySelector(".humidity4").innerHTML = "Humidity: " + data.list[4].main.humidity + "%";
 
   //gets data from api to append to 5th day on browser
-  document.querySelector(".city").innerHTML = data.city.name; 
+  document.querySelector(".city5").innerHTML = data.city.name; 
   document.querySelector(".temperature5").innerHTML = "Temperature: " + Math.round(data.list[1].main.temp) + "°F";
   document.querySelector(".description5").innerHTML = "Description: " + data.list[5].weather[0].description;
   document.querySelector(".wind5").innerHTML = "Wind: " + Math.round(data.list[5].wind.speed) + "Mph";
   document.querySelector(".humidity5").innerHTML = "Humidity: " + data.list[5].main.humidity + "%";
+
+
+// var weatherImg = document.querySelector(".weather-img")
+//   if (data.list[0].weather[0].icon == "01d"){
+//     $(weatherImg).attr('src',"./assets/images/clear.png");
+//   } 
+//   else if (data.list[0].weather[0].icon == "04n"){
+//     $(weatherImg).attr('src',"./assets/images/clouds.png");
+//   }
 }
 
 $("#search-btn").on("click", function () {
